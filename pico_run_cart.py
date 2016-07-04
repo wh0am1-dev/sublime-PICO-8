7109b2b4ec41a7bc5302b77eb7d1474f7310f192
Carlos Aguilar <carlos.aguilar.112358@gmail.com>
2016-07-04 13:58:21 +0200

    fixed #2

--- pico_run_cart.py
+++ pico_run_cart.py
@@ -6,11 +6,11 @@ class PicoRunCartCommand(sublime_plugin.TextCommand):
 		def target():
 			subprocess.Popen(self.cmd, bufsize=-1, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
-		pico8 = self.view.settings().get("pico-8_path", "undefined")
+		pico8 = "\"" + self.view.settings().get("pico-8_path", "undefined") + "\""
 		if pico8 == "undefined":
 			sublime.error_message("Error: \"pico-8_path\" is not defined !\n\nRun \"PICO-8: Setup Path\" from the Command Palette.")
 			return
 
-		cart = self.view.file_name()
+		cart = "\"" + self.view.file_name() + "\""
 		self.cmd = pico8 + " -run " + cart
 		threading.Thread(target=target).start()
